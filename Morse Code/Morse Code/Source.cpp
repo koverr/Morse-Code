4d06af2682c383ee30d1535688c2a40b0a2450b3
Kory Overbay <kory.overbay@gmail.com>
2017-11-27 18:19:22 -0600

    Moved Create/Merge

--- Morse Code/Morse Code/Source.cpp
+++ Morse Code/Morse Code/Source.cpp
@@ -3,7 +3,45 @@
 
 using namespace std;
 
+Binary_Tree<char> create(std::ifstream& fin);
+
 int main() {
-	
-	
+
+}
+
+Binary_Tree<char> create(std::ifstream& fin) {
+	char letter;
+	string morse;
+	map<char, string> mMap;
+	Binary_Tree<char> mTree;
+	BTNode<char>* root;
+	BTNode<char>* ptr;
+	string::iterator iter;
+
+	root = new BTNode<char>(' ');
+	mTree.setRoot(root);
+
+	while (!fin.eof()) {
+		ptr = root;
+		fin >> letter;
+		getline(fin, morse);
+		mMap[letter] = morse;
+
+		iter = morse.begin();
+		while (iter != morse.end()) {
+			if (*iter == '.') {
+				if (ptr->left == NULL)
+					ptr->left = new BTNode<char>(' ');
+				ptr = ptr->left;
+			}
+			else {
+				if (ptr->right == NULL)
+					ptr->right = new BTNode<char>(' ');
+				ptr = ptr->right;
+			}
+			iter++;
+		}
+		ptr->data = letter;
+	}
+	return mTree;
 }
